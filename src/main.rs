a691730267fb606cf0dc30682c0d1e6c827668cc
KMConner <KMConner@users.noreply.github.com>
2022-11-13 20:43:32 +0900

    Implement main

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,35 @@ mod digit;
 mod serial;
 
 fn main() {
-    println!("Hello, world!");
+    let serial_connection = serialport::new("/dev/ttyS0", 9600).open().unwrap();
+    let mut flexi_connection = serial::FlexiConnection::new(serial_connection);
+    loop {
+        let packet = match flexi_connection.read_packet() {
+            Ok(data) => {
+                data
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                continue;
+            }
+        };
+        if packet.len() == 0 {
+            println!("empty packet");
+            continue;
+        }
+
+        if packet[0] != 0x12 || packet.len() != 4 {
+            continue;
+        }
+
+        let digit_bin: [u8; 3] = [packet[1], packet[2], packet[3]];
+        let height = match digit::parse(&digit_bin) {
+            Ok(h) => h,
+            Err(e) => {
+                println!("{:?}", e);
+                continue;
+            }
+        };
+        println!("Current height: {}", height);
+    }
 }
