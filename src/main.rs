cee62ad16f862389e9d8c83bae62628e089bcbcb
KMConner <KMConner@users.noreply.github.com>
2022-12-01 00:42:48 +0900

    export prometheus

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,51 @@
 extern crate core;
 
-use std::sync::Arc;
-use std::sync::atomic::{AtomicU32, Ordering};
-use std::sync::mpsc::channel;
-use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use metrics::{gauge, register_gauge};
+use metrics_exporter_prometheus::PrometheusBuilder;
 
 mod digit;
 mod serial;
 
 fn main() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080);
+    register_gauge!("desk_height");
+    PrometheusBuilder::new().with_http_listener(addr).install().unwrap();
+
     let serial_connection = serialport::new("/dev/ttyS0", 9600).open().unwrap();
     let mut flexi_connection = serial::FlexiConnection::new(serial_connection);
 
-    let (stop_receive_tx, stop_receive_rx) = channel();
-    let (stop_export_tx, stop_export_rx) = channel();
-    let height =Arc::new( AtomicU32::new(0));
-    let height2 = height.clone();
-
-    let receiver_handle = thread::spawn(move || {
-        loop {
-            if stop_receive_rx.try_recv().is_ok() {
-                break;
+    loop {
+        let packet = match flexi_connection.read_packet() {
+            Ok(data) => {
+                data
             }
-            let packet = match flexi_connection.read_packet() {
-                Ok(data) => {
-                    data
-                }
-                Err(serial::Error::DeviceTurnedOffError) => {
-                    continue;
-                }
-                Err(e) => {
-                    println!("{:?}", e);
-                    continue;
-                }
-            };
-            if packet.len() == 0 {
-                println!("empty packet");
+            Err(serial::Error::DeviceTurnedOffError) => {
                 continue;
             }
-
-            if packet[0] != 0x12 || packet.len() != 4 {
+            Err(e) => {
+                println!("{:?}", e);
                 continue;
             }
-
-            let digit_bin: [u8; 3] = [packet[1], packet[2], packet[3]];
-            let new_height = match digit::parse(&digit_bin) {
-                Ok(h) => h,
-                Err(e) => {
-                    println!("{:?}", e);
-                    continue;
-                }
-            };
-            height.store(new_height,Ordering::Relaxed);
+        };
+        if packet.len() == 0 {
+            println!("empty packet");
+            continue;
         }
-    });
 
-    let export_thread = thread::spawn(move || {
-        let mut latest_height = 0;
-        loop {
-            if stop_export_rx.try_recv().is_ok() {
-                break;
+        if packet[0] != 0x12 || packet.len() != 4 {
+            continue;
+        }
+
+        let digit_bin: [u8; 3] = [packet[1], packet[2], packet[3]];
+        let new_height = match digit::parse(&digit_bin) {
+            Ok(h) => h,
+            Err(e) => {
+                println!("{:?}", e);
+                continue;
             }
-            latest_height= height2.load(Ordering::Relaxed);
-            println!("height: {}", latest_height);
-            thread::sleep(Duration::from_millis(500));
-        }
-    });
-
-    sleep(Duration::from_secs(30));
-    stop_receive_tx.send(()).unwrap();
-    stop_export_tx.send(()).unwrap();
-    receiver_handle.join().unwrap();
-    export_thread.join().unwrap();
+        };
+        gauge!("desk_height", new_height as f64);
+    }
 }
